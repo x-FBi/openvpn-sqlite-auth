360ed96421d0ba3946ac0c9361b9fb85e0817a6d
x-FBi <36416072+x-FBi@users.noreply.github.com>
2021-01-09 21:02:09 -0500

    Update user-auth.py

--- user-auth.py
+++ user-auth.py
@@ -6,18 +6,20 @@ import os
 import sqlite3
 import sys
 
-from config import DB_PATH, HASH_ALGORITHM
+from config import DB_PATH, HASH_ALGORITHM, HASH_SALTY
 
 
 hash_func = getattr(hashlib, HASH_ALGORITHM)
 conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
+salty = hash_func(HASH_SALTY.encode("utf-8")).hexdigest()
 
 cursor.execute('SELECT * FROM users WHERE username = ?;', (os.environ['username'],))
 result = cursor.fetchone()
 if result is None:
     sys.exit(1)
 username, password = result
-if hash_func(os.environ['password'].encode("utf-8")).hexdigest() != password:
+passw = (os.environ['password'] + salty) #Salt this mofo
+if hash_func(passw.encode("utf-8")).hexdigest() != password:
     sys.exit(1)
 sys.exit(0)
