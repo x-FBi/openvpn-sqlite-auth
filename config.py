bb50efbb44e3c3ced44f7c80bbebbe1f624c95ed
x-FBi <36416072+x-FBi@users.noreply.github.com>
2021-01-09 20:53:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # Path where users database should be stored
-DB_PATH = '/tmp/openvpn-auth-test.sqlite'
+DB_PATH = '/etc/openvpn/test.sqlite'
 # Minimum required length for passwords when creating users
-PASSWORD_LENGTH_MIN = 8
+PASSWORD_LENGTH_MIN = 5
 # Hash algorithm to use for passwords storage. Can be one of:
 # md5, sha1, sha224, sha256, sha384, sha512
-HASH_ALGORITHM = 'sha256'
+HASH_ALGORITHM = 'sha512'
+# Add a SALT to the password
+HASH_SALTY = ''
