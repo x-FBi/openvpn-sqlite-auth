0617f71d10ef1598f9f36c801ee5c59cd6517bfc
x-FBi <36416072+x-FBi@users.noreply.github.com>
2021-04-04 20:22:21 -0400

    Update createdb.py
    Creates Database,
    Table,
    Column usernames, and passwords

--- python-mysql/createdb.py
+++ python-mysql/createdb.py
@@ -17,6 +17,22 @@ DB = "CREATE DATABASE IF NOT EXISTS %s" % DB_NAME # Create database, unless it e
 try:
     cursor.execute(DB)
     print(f"* Created Database %s" % DB_NAME)
+    connection.close()
 except database.Error as e:
     print(f"Error Creating Database: {e} ")
     sys.exit
+
+connection = database.connect(
+    user=DB_ADMIN,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    database=DB_NAME,
+)
+
+cursor = connection.cursor()
+
+try:
+    cursor.execute("CREATE TABLE users (std_id int, usernames varchar(100), passwords varchar(250));")
+    print(f"Table USERS, with column usernames, and passwords")
+except database.Error as e:
+    print(f"Error: {e} ")
