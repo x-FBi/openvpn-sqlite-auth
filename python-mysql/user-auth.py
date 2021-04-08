3608c0031d74ed7d6b9dbb742d2643d250198290
FBi <36416072+x-FBi@users.noreply.github.com>
2021-04-08 19:52:03 -0400

    Update user-auth.py
    Should /actually/ be working now.

--- python-mysql/user-auth.py
+++ python-mysql/user-auth.py
@@ -4,6 +4,7 @@
 import os
 import mysql.connector as database
 import sys
+import hashlib
 from config import DB_NAME, DB_ADMIN, DB_PASSWORD, DB_HOST, HASH_ALGORITHM, HASH_SALTY
 
 connection = database.connect(
@@ -18,15 +19,17 @@ cursor = connection.cursor()
 hash_func = getattr(hashlib, HASH_ALGORITHM)
 salty = hash_func(HASH_SALTY.encode("utf-8")).hexdigest()
 
-ovpnuser = os.environ['username']
-
-cursor.execute('SELECT * FROM users WHERE username = ?;', (os.environ['username'],))
+ovpnuser = (os.environ['username'])
+ovpnuser = "SELECT * FROM users WHERE usernames = '" + ovpnuser + "';"
+print(ovpnuser)
+cursor.execute(ovpnuser)
 result = cursor.fetchone()
 
 if result is None:
     sys.exit(1)
-username, password = result
+print(result)
+id, usernames, passwords = result
 passw = (os.environ['password'] + salty) #Salt this mofo
-if hash_func(passw.encode("utf-8")).hexdigest() != password:
+if hash_func(passw.encode("utf-8")).hexdigest() != passwords:
     sys.exit(1)
 sys.exit(0)
