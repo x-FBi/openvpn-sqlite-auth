84bc587a982c1a9f7d51fc487d912ce4e7ebc42f
FBi <36416072+x-FBi@users.noreply.github.com>
2021-05-15 13:21:03 -0400

    Working -- But in debugging mode.
    This will not fail any user/password at the moment.  It's in debugging to figure out why it wasn't working -- I found a typo earlier, but I don't believe that was everything.  still working on it.

--- python-mysql/test-user-auth.py
+++ python-mysql/test-user-auth.py
@@ -19,31 +19,30 @@ cursor = connection.cursor()
 hash_func = getattr(hashlib, HASH_ALGORITHM)
 salty = hash_func(HASH_SALTY.encode("utf-8")).hexdigest()
 
-ovpnuser = os.environ.get('username')
-testuser = ovpnuser
+#ovpnuser = (os.environ['username'])
+ovpnuser = os.environ.get('username', 'test') # ovpn uses 'username'  and 'test' is default if not found
 ovpnuser = "SELECT * FROM users WHERE usernames = '" + ovpnuser + "';"
 print(ovpnuser)
 cursor.execute(ovpnuser)
 result = cursor.fetchone()
-f = open('/var/log/auth-error.log', 'a')
-f.write("SQL: " + ovpnuser)
-f.write("OVPN User: " + testuser)
-f.close()
 
 if result is None:
+    print(f"No User Found in: " + ovpnuser + " \n")
     sys.exit(1)
 
 id, usernames, passwords = result
-passw = (os.environ['password'] + salty) #Salt this mofo
+passw = (os.environ.get('password') + salty) #Salt this mofo
+#passw = (os.environ['password'] + salty) #Salt this mofo
 #print(passwords)
 #print(passw)
-f = open('/var/log/auth-error.log', 'a')
+f = open('/root/auth-error.log', 'a')
 f.write("passwords: " + passwords)
-f.write("passw: " + passw)
-f.write("Encoded: " + hash_func(passw.encode("utf-8")).hexdigest())
+f.write("\npassw: " + passw)
+f.write("\nEncoded: " + hash_func(passw.encode("utf-8")).hexdigest() + "\n")
 f.close()
 if hash_func(passw.encode("utf-8")).hexdigest() != passwords:
-    print(f"DENIED: " + p)
+    print(f"DENIED: " + passw)
     sys.exit(1)
+
 print(f"Success!")
 sys.exit(0)
