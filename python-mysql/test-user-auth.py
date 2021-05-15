df5bcf23bef1f16cf41ba911addff5e90a66d432
FBi <36416072+x-FBi@users.noreply.github.com>
2021-05-15 13:37:39 -0400

    WORKING!
    Don't mind all the extras -- those will be erased.
    Nonetheless, it's working!

--- python-mysql/test-user-auth.py
+++ python-mysql/test-user-auth.py
@@ -15,34 +15,21 @@ connection = database.connect(
 )
 cursor = connection.cursor()
 
-
 hash_func = getattr(hashlib, HASH_ALGORITHM)
 salty = hash_func(HASH_SALTY.encode("utf-8")).hexdigest()
 
-#ovpnuser = (os.environ['username'])
 ovpnuser = os.environ.get('username', 'test') # ovpn uses 'username'  and 'test' is default if not found
 ovpnuser = "SELECT * FROM users WHERE usernames = '" + ovpnuser + "';"
-print(ovpnuser)
 cursor.execute(ovpnuser)
 result = cursor.fetchone()
 
-if result is None:
-    print(f"No User Found in: " + ovpnuser + " \n")
+if result is None: # User doesn't exist
     sys.exit(1)
 
 id, usernames, passwords = result
 passw = (os.environ.get('password') + salty) #Salt this mofo
-#passw = (os.environ['password'] + salty) #Salt this mofo
-#print(passwords)
-#print(passw)
-f = open('/root/auth-error.log', 'a')
-f.write("passwords: " + passwords)
-f.write("\npassw: " + passw)
-f.write("\nEncoded: " + hash_func(passw.encode("utf-8")).hexdigest() + "\n")
-f.close()
+
 if hash_func(passw.encode("utf-8")).hexdigest() != passwords:
-    print(f"DENIED: " + passw)
     sys.exit(1)
 
-print(f"Success!")
 sys.exit(0)
